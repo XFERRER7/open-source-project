708660aa6fc169108046af6aa3e0544f03a87420
Alexandre Talles <alexandretalles777@gmail.com>
2023-06-20 22:48:53 -0300

    provider changed

--- prisma/migrations/20230620231829_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Phrase" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "text" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
